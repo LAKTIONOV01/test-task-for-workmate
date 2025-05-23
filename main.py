ad3bd6948d85cd73a4bab64d15aa0648c6e1e65e
Pavel Laktionov <143647573+LAKTIONOV01@users.noreply.github.com>
2025-05-23 11:25:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-from fastapi import APIRouter
-from typing import Annotated
-from fastapi.security import HTTPBasicCredentials, HTTPBasic
 
-router = APIRouter(prefix='/demo-auth', tags=['Demo Auth'])
-
-@router.get('/basic-auth/')
-def demo_basic_auth_credentials(
-        credentials: Annotated[]
-)
